4a58e29a05b94fd8c97cdfc69be45ee568d20b99
Matteo <matteosabbatini23@gmail.com>
2026-01-27 00:30:37 +0100

    useReducer hook

--- hook-use-reducer/app/page.tsx
+++ hook-use-reducer/app/page.tsx
@@ -1,21 +1,47 @@
 'use client'
-import React, {useState} from "react";
+import React, {useState , useReducer} from "react";
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'    
+}
+
+//   const ERRORS = {
+//   GENERIC: 'An eerror has occurred.'
+// }
+function reducerFunction(state: { count: number; }, action: { type: string; }) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return {count: state.count + 1};
+    case ACTIONS.DECREMENT:
+      return {count: state.count - 1};
+    default:
+      return state;
+      //throw new Error(ERRORS.GENERIC);
+  }
+}
+
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducerFunction, {count: 0});
+
 
   function increment() {
-    setCount(prevCount => prevCount + 1);
+    // setCount(prevCount => prevCount + 1);
+    dispatch({type: ACTIONS.INCREMENT});
   }
   function decrement() {
-    setCount(prevCount => prevCount - 1);
+    // setCount(prevCount => prevCount - 1);
+    dispatch({type: ACTIONS.DECREMENT}); 
   }
+  
+
   return (
     <html suppressHydrationWarning>
       <body suppressHydrationWarning >
-        
-      <h1>Count: {count}</h1>
-      
+
+      <h1>Count: {state.count}</h1>
       <div className="container" >
         <button onClick={increment}>+</button>
         

7cd8665ae35eff1365ae4f670661fcc179a9827e
Matteo <matteosabbatini23@gmail.com>
2026-02-01 23:58:26 +0100

    save "name" in console

--- hook-use-reducer/app/todo-list/page.tsx
+++ hook-use-reducer/app/todo-list/page.tsx
@@ -1,8 +1,17 @@
 'use client'
 import React, {useReducer, useState} from "react";
 
-function reducer(state, action) {
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name) ];
+    default:
+      return todos;
+  }
+}
 
+function newTodo(name: string) {
+  return {id: Date.now(), name: name, complete: false};
 }
 
 const ACTIONS = {
@@ -16,16 +25,17 @@ export default function TodoList() {
 
     function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
-        dispatch({type: ACTIONS.ADD_TODO, payload: name});
+        dispatch({type: ACTIONS.ADD_TODO, payload: {name: name}});
         setName('');
     }
 
+    console.log(todos);
 
   return (
     <html suppressHydrationWarning>
       <body >
         <h1 className="todo">Todo List</h1>
-        <div className="container todo_container">
+        <div className="todo_container">
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="New Todo" value={name} onChange={e => setName(e.target.value)}/>
                 <button type="submit">Add Todo</button>
